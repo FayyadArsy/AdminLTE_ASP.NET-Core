29b7d73c4b6a8bdf7b27bf0ea3980d101435ee17
FayyadArsy <fayyadarsynandi2@gmail.com>
2023-11-09 14:08:08 +0700

    update front end chart

---
+++ ClientNew/Controllers/ChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientNew.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
